5ca376d2852e69c85844c461354bfc1d2d4fdbde
RahmadinaIkaP <rahmadina.ika@student.umn.ac.id>
2023-03-10 13:07:57 +0700

    commit first project

---
+++ app/src/main/java/id/ac/umn/rahmadina/app_diagnosa_feline_skripsi/view/splashscreen/SplashScreenFragment.kt
@@ -0,0 +1,33 @@
+package id.ac.umn.rahmadina.app_diagnosa_feline_skripsi.view.splashscreen
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.ac.umn.rahmadina.app_diagnosa_feline_skripsi.R
+import id.ac.umn.rahmadina.app_diagnosa_feline_skripsi.databinding.FragmentSplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenFragment : Fragment() {
+    private var _binding : FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
