b95e7ecabf7a2962db4e16d9c1488c905a314c3f
RahmadinaIkaP <rahmadina.ika@student.umn.ac.id>
2023-03-17 12:12:23 +0700

    add layout history, confirm diagnose, input cat name form

---
+++ app/src/main/java/id/ac/umn/rahmadina/app_diagnosa_feline_skripsi/view/diagnosis/FormCatNameFragment.kt
@@ -0,0 +1,32 @@
+package id.ac.umn.rahmadina.app_diagnosa_feline_skripsi.view.diagnosis
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.ac.umn.rahmadina.app_diagnosa_feline_skripsi.R
+import id.ac.umn.rahmadina.app_diagnosa_feline_skripsi.databinding.FragmentFormCatNameBinding
+
+
+class FormCatNameFragment : Fragment() {
+    private var _binding: FragmentFormCatNameBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFormCatNameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
